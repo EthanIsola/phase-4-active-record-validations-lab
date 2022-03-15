fd015fbc4af89e672ff822b4f0d38e90f24f652b
Ethan Isola <isolaethan@gmail.com>
2022-03-15 15:53:33 -0500

    commit tree e345545051c85a7456ecb7739dee3343e9ef3afe

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum:250 }
+    validates :summary, length: { maximum:250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "is not included in the list" }
+    validate :clickbait
+
+    def clickbait
+        if title.present? && (["Won't Believe","Secret","Top","Guess"].any? { |word| title.include?(word) }) != true
+            errors.add(:title, "It can't")
+        end
+    end
 end
